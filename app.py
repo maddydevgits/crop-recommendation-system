2be41f298421bf51b7d95b840c8011b9da212dfc
MS <72651078+maddydevgits@users.noreply.github.com>
2024-12-21 22:29:45 +0530

    Revert "removed flash"
    This reverts commit 719bd6424c8cbfcc5e5ef27918419e962c90fa65.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Importing essential libraries and modules
 
-from flask import Flask, render_template, request,redirect,url_for,flash,session
+from flask import Flask, render_template, request,redirect,url_for,flash
 from markupsafe import Markup
 import numpy as np
 import pandas as pd
@@ -288,10 +288,12 @@ def register():
         role = request.form.get('role')
 
         if not name or not email or not password or not role:
+            flash('All fields are required!', 'danger')
             return redirect(url_for('register'))
         
         existing_user = users_collection.find_one({"email": email})
         if existing_user:
+            flash('Email already exists!', 'danger')
             return redirect(url_for('register'))
 
         hashed_password = hash_password(password)
@@ -303,9 +305,13 @@ def register():
         }
 
         users_collection.insert_one(new_user)
+        flash('Registration successful!', 'success')
         return redirect(url_for('login'))
 
     return render_template('register.html')  # Render the registration form
+
+# Login route
+from flask import session
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
